1b29de8f3160dfec6af5778de5b245a34f00477f
Janaina <jmtannus@gmail.com>
2022-09-02 18:49:33 -0300

    adicionando eventos e lógicas

--- main.js
+++ main.js
@@ -1,34 +1,38 @@
 //reproduzir o som que está dentro da tag áudio:
-function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
 }
-//referencia constante(lista que não muda:const, lista que muda, variavel: let):
-const listaDeTeclas = document.querySelectorAll('.tecla');
+
+const listaDeTeclas = document.querySelectorAll('.tecla'); //referencia constante(lista que não muda:const, lista que muda, variavel: let):
 
 //para
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
     const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1];   
+    const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`; //template string
 
-    //jana e linda
- 
     tecla.onclick = function () {
         tocaSom(idAudio);
-    };
+    }
 
     tecla.onkeydown = function (evento) {
 
-        console.log(evento)
-
-        if('Espaço'){
-            tecla.classList.add('ativa');  
-        }        
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
     }
 
     tecla.onkeyup = function () {
         tecla.classList.remove('ativa');
-
     }
+
 }
-//aula Condições no código 6:17
