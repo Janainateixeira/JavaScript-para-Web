a582e5477c86cda74843e7b46ea9f51cf99eea08
Janaina <jmtannus@gmail.com>
2022-09-02 13:54:45 -0300

    Teste

--- main.js
+++ main.js
@@ -10,6 +10,8 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];   
     const idAudio = `#som_${instrumento}`; //template string
+
+    //jana e linda
  
     tecla.onclick = function () {
         tocaSom(idAudio);
